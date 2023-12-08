338d8207de14654b0ef42bdd590a07759e3df555
fatemehprb <fatemepoorbagheri1382@yahoo.com>
2023-12-09 01:22:38 +0330

    main.cpp was added

---
+++ main.cpp
@@ -0,0 +1,48 @@
+﻿#include"vector.h"
+#include<iostream>
+int main() {
+	int initialValues[] = { 5, 2, 9, 1, 5 };
+	Vector myVector(5, initialValues);
+
+	/*myVector.display();
+
+	myVector.push_back(7);
+	myVector.display();
+
+	myVector.pop_back();
+	myVector.display();
+
+	myVector.insert(2, 8);
+	myVector.display();
+
+	myVector.erase(1);
+	myVector.display();
+
+	myVector.erase(1, 4);
+	myVector.display();
+
+	std::cout << "Is vector empty? " << (myVector.empty() ? "Yes" : "No") << std::endl;
+
+	std::cout << "First element: " << myVector.front() << std::endl;
+
+	std::cout << "Last element: " << myVector.last() << std::endl;
+
+	myVector.push_back(3);
+	myVector.push_back(1);
+	myVector.display();
+
+	myVector.swap(0, 2);
+	myVector.display();
+
+	myVector.sort();
+	myVector.display();
+
+	myVector.sort(false);
+	myVector.display();
+
+
+	myVector.sort(1);
+	myVector.display();
+*/
+	return 0;
+}
